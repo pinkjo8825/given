16b8b6cb3a5f3ea9a0ce48c8cd6fe912d1d3c3a1
pinkjo8825 <83690209+pinkjo8825@users.noreply.github.com>
2022-11-10 18:07:54 +0700

    make it better

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-form @submit.prevent="onSubmit" @reset="onReset" class="q-gutter-md">
+    <div>
+      <q-input filled v-model="todo" type="text" label="Type your list" />
+    </div>
+    <div>
+      <q-btn label="Add" type="submit" color="primary" />
+      <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+    </div>
+  </q-form>
+</template>
+<script>
+import { useTodoListStore } from "../stores/useTodoListStore";
+export default {
+  name: "TodoForm",
+  data() {
+    return {
+      todo: "",
+      store: useTodoListStore(),
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.todo.length == 0) return;
+      this.store.addTodo(this.todo);
+      this.todo = "";
+    },
+    onReset() {
+      this.todo = "";
+    },
+  },
+};
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <TodoForm />
+    <TodoList />
+  </q-page>
+</template>
+
+<script>
+import TodoForm from "../components/TodoForm.vue";
+import TodoList from "../components/TodoList.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "IndexPage",
+  components: {
+    TodoForm,
+    TodoList,
+  },
+});
+</script>
